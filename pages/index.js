6de8a170f39251e32e8696907a1f0db8486c88ed
Jeroen Nelen <jwnelen@gmail.com>
2023-05-24 14:25:14 +0200

    Some more subscriptions

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head';
-import data from '../data/subscriptions.json';
+import importedData from '../data/subscriptions.json';
 import Footer from '../components/footer';
 import { useState } from 'react';
 
 export default function Home() {
-  const [selected, setSelected] = useState(["Netflix"]);
+  const [data, setData] = useState(importedData);
+  const [selected, setSelected] = useState([]);
 
   const currentTotal = selected.reduce((acc, curr) => {
     const subscription = data.find(item => item.name === curr);
@@ -19,35 +20,37 @@ export default function Home() {
       </Head>
 
       <main className='text-center m-2'>
-        <h1 className='text-3xl mb-10'>
+        <h1 className='text-3xl my-10'>
           The Subscription Fatigue 
         </h1>
 
-        <div className='flex'>
-        {data.map( (subscription, index) => (
-          <div className={`w-96 flex flex-col 
-                m-2 p-2 ${selected.includes(subscription.name) ? 'bg-blue-300' : 'bg-white'}
-                items-center justify-between border`}
-                onClick={() => {
-                  if (selected.includes(subscription.name)) {
-                    setSelected(selected.filter(item => item !== subscription.name))
-                  } else {
-                    setSelected([...selected, subscription.name])
-                  }
-                }}
-            key={index}>
-            <h2>{subscription.name}</h2>
-            <div className='w-12'>
-              <img src={subscription.img_link} alt={subscription.name} />
+        <div className='container mx-auto'>
+          <div className='flex flex-wrap'>
+          {data.map( (subscription, index) => (
+            <div className={`flex flex-col w-1/4 h-32 m-4
+                  p-2 ${selected.includes(subscription.name) ? 'bg-blue-300' : 'bg-white'}
+                  items-center justify-between border`}
+                  onClick={() => {
+                    if (selected.includes(subscription.name)) {
+                      setSelected(selected.filter(item => item !== subscription.name))
+                    } else {
+                      setSelected([...selected, subscription.name])
+                    }
+                  }}
+              key={index}>
+              <h2 className='text-xl'>{subscription.name}</h2>
+              <div className='w-12'>
+                <img src={subscription.img_link} alt={subscription.name} />
+              </div>
+
+              <p>{subscription.std_price}</p>
+
+            </div>)
+            )}
             </div>
-
-            <p>{subscription.std_price}</p>
-
-          </div>)
-          )}
           </div>
 
-          <div>
+          <div className='text-xl'>
             Total: {currentTotal}
           </div>
         
