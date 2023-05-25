2f41236a3f4acfa29e6a2f7836340dbe8062fa7d
Jeroen Nelen <jwnelen@gmail.com>
2023-05-25 22:25:58 +0200

    Working on changing the price

---
+++ components/subscription.jsx
@@ -0,0 +1,29 @@
+
+
+const Subscription = ({
+    subscription,
+    changePrice,
+    selected,
+    index,
+    onClick}
+) => {
+    return (
+    <div className={`flex flex-row items-center justify-between
+        cursor-pointer ml-8
+        rounded border p-2 m-4 outline outline-1 
+        ${selected ? 'outline-4 outline-blue-500' : 'outline-gray-400'}
+        `}
+        key={index}
+        onClick={onClick}
+        >
+            <h2 className='text-xl'>{subscription.name}</h2>
+            {/* <div className=''>
+                <img className='object-contain' height={10} width={100} src={subscription.img_link} alt={subscription.name} />
+            </div> */}
+            <input value={subscription.std_price}
+                    onChange={(e) => changePrice(e.target.value)}></input>
+        <p>{subscription.std_price}</p>
+    </div>
+    )
+}
+export default Subscription
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import importedData from '../data/subscriptions.json';
 import Footer from '../components/footer';
+import Subscription from '../components/subscription'
 import { useState } from 'react';
 
 export default function Home() {
@@ -31,30 +32,26 @@ export default function Home() {
           {data.map((category, index) => {
             return (
               <div>
-            <h1 className='text-xl'>{category.category}</h1>
+            <h1 className='text-xl text-left'>{category.category}</h1>
+            
             {category.items?.map((subscription, index) => {
+              const s = selected.includes(subscription.name)
               return (
-                <div className={`flex flex-row items-center justify-between
-                  cursor-pointer
-                  rounded border p-2 m-4 outline outline-1 
-                  ${selected.includes(subscription.name) ? 'outline-4 outline-blue-500' : 'outline-gray-400'}
-                   `}
-                  key={index}
-                    onClick={() => {
-                      if (selected.includes(subscription.name)) {
-                        setSelected(selected.filter(item => item !== subscription.name))
-                      } else {
-                        setSelected([...selected, subscription.name])
-                      }
-                    }}
-                    >
-                      <h2 className='text-xl'>{subscription.name}</h2>
-                      {/* <div className=''>
-                        <img className='object-contain'  width={120} height={30} src={subscription.img_link} alt={subscription.name} />
-                      </div> */}
-                  <p>{subscription.std_price}</p>
-                </div>
-              )
+              <Subscription
+                subscription={subscription}
+                index={index}
+                selected={s}
+                changePrice={(e) => {
+                  console.log(e)}
+                }
+                onClick={() => {
+                  if (s) {
+                    setSelected(selected.filter(item => item !== subscription.name))
+                    } else {
+                    setSelected([...selected, subscription.name])
+                    }
+                }}
+                />)
             })}
             </div>
             )})
