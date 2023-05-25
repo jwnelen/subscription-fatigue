788c8cfb21114d05a7630ec9c014e22405242d70
Jeroen Nelen <jwnelen@gmail.com>
2023-05-25 22:05:41 +0200

    Into list and categories

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,12 @@ import { useState } from 'react';
 export default function Home() {
   const [data, setData] = useState(importedData);
   const [selected, setSelected] = useState(["Spotify"]);
-
+  // const currentTotal = 0
+  const l = data.map(x => x.items)
+  const fl = l.flat()
+  // console.log(l.flat())
   const currentTotal = selected.reduce((acc, curr) => {
-    const subscription = data.find(item => item.name === curr);
+    const subscription = fl.find(item => item.name === curr);
     return acc + subscription.std_price;
   }, 0);
 
@@ -25,33 +28,38 @@ export default function Home() {
         </h1>
 
         <div className='container mx-auto'>
-          <div className='flex flex-wrap justify-center'>
-          {data.map( (subscription, index) => (
-            <div className={`flex flex-col items-center justify-between
-                  rounded border p-2 w-64 h-40 m-4 outline outline-1 
+          {data.map((category, index) => {
+            return (
+              <div>
+            <h1 className='text-xl'>{category.category}</h1>
+            {category.items?.map((subscription, index) => {
+              return (
+                <div className={`flex flex-row items-center justify-between
+                  cursor-pointer
+                  rounded border p-2 m-4 outline outline-1 
                   ${selected.includes(subscription.name) ? 'outline-4 outline-blue-500' : 'outline-gray-400'}
                    `}
-              key={index}>
-                <div className='w-100 h-full p-2 flex flex-col cursor-pointer items-center justify-between'
-                onClick={() => {
-                  if (selected.includes(subscription.name)) {
-                    setSelected(selected.filter(item => item !== subscription.name))
-                  } else {
-                    setSelected([...selected, subscription.name])
-                  }
-                }}>
-                  <h2 className='text-xl'>{subscription.name}</h2>
-                  <div className=''>
-                    <img className='object-contain'  width={120} height={30} src={subscription.img_link} alt={subscription.name} />
-                  </div>
-              </div>
-              <p>{subscription.std_price}</p>
-
-            </div>)
-            )}
+                  key={index}
+                    onClick={() => {
+                      if (selected.includes(subscription.name)) {
+                        setSelected(selected.filter(item => item !== subscription.name))
+                      } else {
+                        setSelected([...selected, subscription.name])
+                      }
+                    }}
+                    >
+                      <h2 className='text-xl'>{subscription.name}</h2>
+                      {/* <div className=''>
+                        <img className='object-contain'  width={120} height={30} src={subscription.img_link} alt={subscription.name} />
+                      </div> */}
+                  <p>{subscription.std_price}</p>
+                </div>
+              )
+            })}
             </div>
-          </div>
-
+            )})
+            }
+            </div>
           <div className='text-xl'>
             Total: &euro;{currentTotal.toFixed(2)}
           </div>
