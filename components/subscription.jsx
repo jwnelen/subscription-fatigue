e73f0c30e8e32f2fb1690535eeceb55fb1075064
Jeroen Nelen <jwnelen@gmail.com>
2023-06-04 14:21:31 +0200

    Added some functionalities and style

--- components/subscription.jsx
+++ components/subscription.jsx
@@ -8,21 +8,25 @@ const Subscription = ({
     onClick}
 ) => {
     return (
-    <div className={`flex flex-row items-center justify-between
-        cursor-pointer ml-8
-        rounded border p-2 m-4 outline outline-1 
-        ${selected ? 'outline-4 outline-blue-500' : 'outline-gray-400'}
-        `}
-        key={index}
-        onClick={onClick}
-        >
-            <h2 className='text-xl'>{subscription.name}</h2>
-            {/* <div className=''>
-                <img className='object-contain' height={10} width={100} src={subscription.img_link} alt={subscription.name} />
-            </div> */}
-            {/* <input value={subscription.std_price} */}
-                    {/* onChange={(e) => changePrice(e.target.value)}></input> */}
-        <p>{subscription.std_price}</p>
+    <div className={`flex flex-row m-4 gap-4 justify-between outline p-2 rounded
+            ${selected ? 'outline-3 outline-blue-500' : 'outline-gray-100'}`}>
+        <div className={`
+            cursor-pointer flex-1 flex flew-row justify-between items-center
+            
+            `}
+            key={`SUB-index`}
+            onClick={onClick}
+            >
+                <h2 className='text-xl'>{subscription.name}</h2>
+                {/* <div className=''>
+                    <img className='object-contain' height={10} width={100} src={subscription.img_link} alt={subscription.name} />
+                </div> */}
+            <p className="text-gray-200">{subscription.std_price === NaN ? 0 : subscription.std_price}</p>
+            
+        </div>
+        <input value={subscription.std_price} type="number"
+            className='border-0 rounded p-1 w-20 text-right' 
+                        onChange={changePrice}></input>
     </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import importedData from '../data/subscriptions.json';
 import Footer from '../components/footer';
 import Subscription from '../components/subscription'
 import { useState } from 'react';
+import DisplayTotal from '../components/totalDisplay';
 
 export default function Home() {
   const [data, setData] = useState(importedData);
@@ -13,7 +14,7 @@ export default function Home() {
   // console.log(l.flat())
   const currentTotal = selected.reduce((acc, curr) => {
     const subscription = fl.find(item => item.name === curr);
-    return acc + subscription.std_price;
+    return acc + ( isNaN(subscription.std_price) ? 0 : subscription.std_price);
   }, 0);
 
   return (
@@ -23,17 +24,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className='text-center m-2'>
+      <main className='text-center'>
         <h1 className='text-3xl my-10'>
           The Subscription Fatigue 
         </h1>
 
         
-        <div className='container mx-auto'>
+        <div className='container mx-auto h-full mb-20'>
           {data.map((category, index) => {
             return (
               <div key={`s-${index}`}>
-                <h1 className='text-xl text-left'>{category.category}</h1>
+                <h1 className='text-xl font-bold text-center'>{category.category}</h1>
                 <div>
                 {category.items?.map((subscription, index) => {
                   const s = selected.includes(subscription.name)
@@ -42,9 +43,32 @@ export default function Home() {
                     subscription={subscription}
                     index={index}
                     selected={s}
+                    key={`s-${index}`}
                     changePrice={(e) => {
-                      console.log(e)}
-                    }
+                      // Set the price of the subscription
+                      let newPrice = e.target.value
+
+                      try {
+                        newPrice = parseFloat(newPrice)
+                      } catch (error) {
+                        newPrice = subscription.std_price
+                      }
+                      const newData = data.map((cat) => {
+                        return {
+                          ...cat,
+                          items: cat.items.map((sub) => {
+                            if (sub.name === subscription.name) {
+                              return {
+                                ...sub,
+                                std_price: newPrice,
+                              };
+                            }
+                            return sub;
+                          }),
+                        };
+                      })
+                      setData(newData)
+                    }}
                     onClick={() => {
                       if (s) {
                         setSelected(selected.filter(item => item !== subscription.name))
@@ -59,11 +83,10 @@ export default function Home() {
             )})
             }
             </div>
-          <div className='text-xl'>
-            Total: &euro;{currentTotal.toFixed(2)}
-          </div>
-      </main>
 
+        <DisplayTotal total={currentTotal}></DisplayTotal>
+          
+      </main>
       <Footer/>
     </div>
   )
