925ee9c362f77caca68295b73f1605bc9082e62f
Jeroen Nelen <jwnelen@gmail.com>
2023-09-26 10:02:31 +0200

    adding per category

--- pages/index.js
+++ pages/index.js
@@ -18,13 +18,23 @@ export default function Home() {
     return acc + (isNaN(subscription.std_price) ? 0 : subscription.std_price);
   }, 0);
 
-  const add = () => {
-    // setTheArray(oldArray => [...oldArray, newElement]);
-
+  const addCategory = () => {
     setData((d) => [
       ...d,
       {
         category: "New category",
+        name: "New subscription",
+        std_price: 0,
+        id: uuidv4(),
+      },
+    ]);
+  };
+
+  const addSubscription = (cat) => {
+    setData((d) => [
+      ...d,
+      {
+        category: cat,
         name: "New subscription",
         std_price: 0,
         id: uuidv4(),
@@ -46,7 +56,20 @@ export default function Home() {
           {uniqueCategories.map((category, index) => {
             return (
               <div key={`s-${index}`}>
-                <h1 className="text-xl font-bold text-center">{category}</h1>
+                <div className="flex flex-row gap-3 justify-center">
+                  <h1 className="text-xl font-bold text-center">{category}</h1>
+                  <Button onClickCapture={(e) => addSubscription(category)}>
+                    <svg
+                      className="w-6 h-6 text-green-500 dark:text-white"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M9.546.5a9.5 9.5 0 1 0 9.5 9.5 9.51 9.51 0 0 0-9.5-9.5ZM13.788 11h-3.242v3.242a1 1 0 1 1-2 0V11H5.304a1 1 0 0 1 0-2h3.242V5.758a1 1 0 0 1 2 0V9h3.242a1 1 0 1 1 0 2Z" />
+                    </svg>
+                  </Button>
+                </div>
                 <div>
                   {data
                     .filter((x) => x.category === category)
@@ -83,7 +106,7 @@ export default function Home() {
           <div className="mt-20 mb-32 flex justify-center">
             <Button
               className="text-xl p-2 bg-blue-500 rounded"
-              onClick={() => add()}
+              onClick={() => addCategory()}
             >
               Add new subscription
             </Button>
